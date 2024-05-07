4e6873faf7354364609ade6bdbcbc84b6083f645
atirahhusna <atirahhusna873@gmail.com>
2024-05-07 11:26:15 +0800

    login interfase updated

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('Login',LoginController::class);
