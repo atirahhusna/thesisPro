e374921481cba34376ccc0f638e496d73c63971a
atirahhusna <atirahhusna873@gmail.com>
2024-05-12 17:57:10 +0800

    platinum

--- resources/views/MainInterface/test.blade.php
+++ resources/views/MainInterface/test.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PlatinumPage</title>
+    <title>Sidebar With Bootstrap</title>
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
@@ -90,12 +90,6 @@
             display: none;
         }
 
-        #footer{
-      background-color: #ffffff;
-      text-align:justify;
-      padding-top:10px;
-    }
-
         .sidebar-nav {
             padding: 2rem 0;
             flex: 1 1 auto;
@@ -174,6 +168,18 @@
             </div>
             <ul class="sidebar-nav">
                 <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-user"></i>
+                        <span>Profile</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-agenda"></i>
+                        <span>Publication</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
                     <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse"
                         data-bs-target="#auth" aria-expanded="false" aria-controls="auth">
                         <i class="lni lni-protection"></i>
@@ -195,30 +201,38 @@
                     </ul>
                 </li>
                 <li class="sidebar-item">
-                    <a href="#" class="sidebar-link">
-                        <i class="lni lni-agenda"></i>
-                        <span>Publication</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
                     <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse"
-                        data-bs-target="#register" aria-expanded="false" aria-controls="register">
+                        data-bs-target="#multi" aria-expanded="false" aria-controls="multi">
                         <i class="lni lni-layout"></i>
-                        <span>Registration</span>
+                        <span>Multi Level</span>
                     </a>
-                    <ul id="register" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                    <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
                         <li class="sidebar-item">
-                            <a href="#" class="sidebar-link">New Registration</a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="#" class="sidebar-link">View Registration</a>
+                            <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse"
+                                data-bs-target="#multi-two" aria-expanded="false" aria-controls="multi-two">
+                                Two Links
+                            </a>
+                            <ul id="multi-two" class="sidebar-dropdown list-unstyled collapse">
+                                <li class="sidebar-item">
+                                    <a href="#" class="sidebar-link">Link 1</a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="#" class="sidebar-link">Link 2</a>
+                                </li>
+                            </ul>
                         </li>
                     </ul>
                 </li>
                 <li class="sidebar-item">
                     <a href="#" class="sidebar-link">
                         <i class="lni lni-popup"></i>
-                        <span>Expert Information</span>
+                        <span>Notification</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-cog"></i>
+                        <span>Setting</span>
                     </a>
                 </li>
             </ul>
@@ -238,17 +252,49 @@
                         </a>
                     </div>
                 </nav>
-                <a class="navbar-brand" href="#">Welcome To ThesisPro !</a>
+                <a class="navbar-brand" href="#">Home</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul class="navbar-nav">
-                        
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Register
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="http://127.0.0.1:8000/Login">New Register</a></li>
+                                <li><a class="dropdown-item" href=" ">View registered platinum</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Profile
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">My Profile</a></li>
+                                <li><a class="dropdown-item" href="#">Mentor Profile</a></li>
+                                <li><a class="dropdown-item" href="#">Platinum Profile</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Report
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Cluster Report</a></li>
+                                <li><a class="dropdown-item" href="#">Publication report</a></li>
+                                <li><a class="dropdown-item" href="#">Expert report</a></li>
+                            </ul>
+                        </li>
                     </ul>
                 </div>
             </nav>
             <div class="row border ">
+                <div class="col-md-3">
+                    <!-- Sidebar Content Goes Here -->
+                    <h5>Sidebar</h5>
+                </div>
                 <div class="col-md-9">
                     <div class="container border">
                         <div class="card">
@@ -264,38 +310,8 @@
                     </div>
                 </div>
             </div>
-            <div class="row border ">
-                <div class="col-md-9">
-                    <div class="container border">
-                        <div class="card">
-                            <img src="{{ URL('images/Software testing.jpg') }}" alt="Software testing" width="100" height="100">
-                            <div class="card-body">
-                                <div class="container">
-                                    <h5 class="card-title">Card title</h5>
-                                </div>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div id="footer">
-
-<table class="center">
-  <tr>
-    <td class="column"><img src="{{ URL('images/logo.jpg') }}" alt="logo" width="150" height="150" ></td>
-    <td style="width:800px;"><p>THESISPRO is a premier academic platform designed to support postgraduate students in managing and showcasing their scholarly work. Our system offers a comprehensive suite of tools for editing, publishing, and sharing research and publications within expert domains. By facilitating seamless interactions among students, mentors, and staff, THESISPRO aims to enhance academic collaboration and promote excellence in research and education.</p></td>
-  </tr>
-</table>
-<hr>
-<p style="text-align:center;">Copyright &copy; 2024 THESISPRO Corporation. All Rights Reserved.</p>
-
-
-</div>
         </div>
     </div>
-    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
         crossorigin="anonymous"></script>
