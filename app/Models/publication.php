c804657b298db8335bc07df07a251a737b326630
jicoon1706 <165003046+jicoon1706@users.noreply.github.com>
2024-05-12 16:14:29 +0800

    publicationModel

--- app/Models/publication.php
+++ app/Models/publication.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PublicationReport extends Model
+class publication extends Model
 {
     use HasFactory;
 }
