69e9ea3a0a5b192cd4f4738cd6bf76a64c15e4e1
egzyyy <ariffnorjihan@gmail.com>
2024-05-13 15:05:03 +0800

    Update WeeklyFocus.blade.php
    tyty

--- resources/views/ProgressMonitoring/WeeklyFocus.blade.php
+++ resources/views/ProgressMonitoring/WeeklyFocus.blade.php
@@ -45,57 +45,6 @@
         }
     </style>
 </head>
-<body class="p-3 m-0 border-0 bd-example m-0 border-0">
-<div class="container-fluid">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-<nav class="navbar navbar-expand-lg navbar-light bg-light" style="margin-left: 150px; margin-right: 300px;" ><img src="{{ URL('images/logo.jpg') }}" alt="logo" width="80" height="80"></nav>
-    <a class="navbar-brand" href="#">Home</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-      <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Register
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="http://127.0.0.1:8000/Login">New Register</a></li>
-            <li><a class="dropdown-item" href="#">View registered platinum</a></li>
-          </ul>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Profile
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">My Profile</a></li>
-            <li><a class="dropdown-item" href="#">Mentor Profile</a></li>
-            <li><a class="dropdown-item" href="#">Platinum Profile</a></li>
-          </ul>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-           Report
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Cluster Report</a></li>
-            <li><a class="dropdown-item" href="#">Publication report</a></li>
-            <li><a class="dropdown-item" href="#">Expert report</a></li>
-          </ul>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-           Progress Monitoring
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Weekly Focus</a></li>
-            <li><a class="dropdown-item" href="#">Draft Thesis Performance</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
     <div class="button-group">
         <div class="button all active">All</div>
         <div class="button focus">Focus</div>
