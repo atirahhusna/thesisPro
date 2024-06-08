790d9dc07a73b02de21c11b7b9327101b3fc3b2b
jicoon1706 <165003046+jicoon1706@users.noreply.github.com>
2024-06-09 00:16:43 +0800

    akmal

---
+++ database/migrations/2024_05_02_034950_create__expert_domain.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('expertdomain', function (Blueprint $table) {
+            $table->id('e_ID');
+           // $table->unsignedBigInteger('platID');
+            //$table->foreign('platID')->references('platID')->on('');
+            $table->string('e_Name', 100);
+            $table->string('e_Email', 50);
+            $table->string('e_PhoneNum');
+            $table->string('e_TitleResearch', 100);
+            $table->string('e_Paper', 100);
+            $table->string('e_University', 100);
+            $table->string('e_Expertise', 100);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('expertdomain');
+    }
+};
